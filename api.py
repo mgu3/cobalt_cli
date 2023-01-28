ea8635862690d5d4a6bf6a327309931597268492
Mark Guthrie <m@rkguthrie.com>
2023-01-28 16:18:43 +1100

    starting

---
+++ api.py
@@ -0,0 +1,54 @@
+import requests
+
+BASE_URL = "https://test.myabf.com.au"
+BASE_URL = "http://127.0.0.1:8000"
+
+
+class CallType:
+    POST = "POST"
+    GET = "GET"
+    PUT = "PUT"
+    DELETE = "DELETE"
+
+
+def cobalt_api_call_post(cobalt_api_key, path, payload):
+    """ Process a POST request """
+
+    return _cobalt_api_call(CallType.POST, cobalt_api_key, path, payload)
+
+
+def cobalt_api_call_get(cobalt_api_key, path, payload):
+    """ Process a GET request """
+
+    return _cobalt_api_call(CallType.GET, cobalt_api_key, path, payload)
+
+
+def cobalt_api_call_put(cobalt_api_key, path, payload):
+    """ Process a PUT request """
+
+    return _cobalt_api_call(CallType.PUT, cobalt_api_key, path, payload)
+
+
+def cobalt_api_call_delete(cobalt_api_key, path, payload):
+    """ Process a DELETE request """
+
+    return _cobalt_api_call(CallType.DELETE, cobalt_api_key, path, payload)
+
+
+def _cobalt_api_call(call_type, cobalt_api_key, path, payload):
+    """ Calls cobalt API and returns status and any response as a python object """
+
+    api_url = f"{BASE_URL}/{path}"
+
+    if call_type == CallType.POST:
+        response = requests.post(api_url, json=payload, headers={"key": cobalt_api_key})
+    elif call_type == CallType.GET:
+        response = requests.get(api_url, json=payload, headers={"key": cobalt_api_key})
+    elif call_type == CallType.PUT:
+        response = requests.put(api_url, json=payload, headers={"key": cobalt_api_key})
+    elif call_type == CallType.DELETE:
+        response = requests.delete(api_url, json=payload, headers={"key": cobalt_api_key})
+    else:
+        raise AttributeError(f"Invalid call type: {call_type}")
+
+    return response.status_code, response.json()
--- cobalt_cli.py
+++ cobalt_cli.py
@@ -1,24 +1,40 @@
 import click
 
+from api import cobalt_api_call_get
 
-# @click.command()
-# def cli():
-#     """Example script."""
-#     click.echo('Hello World!')
+TOKEN = "API_Rm/_XZPkTn2Ez|GYFd)Bj}TPinAhnmXzGwVo"
 
 
 @click.group()
-def cli():
-    pass
+@click.option('--cobalt-api-token', help='Cobalt API Token, can also be set using an environment variable "COBALT_API_TOKEN"')
+def cli(cobalt_api_token):
+    """ Entry point """
+    print("Inside cli")
+    print(cobalt_api_token)
+    print()
+    print()
+
+
 
 
 @cli.command()
-def hello():
+def validate_token():
+    """ Check that a token is valid """
+
+    status, response = cobalt_api_call_get(TOKEN, "api/cobalt/keycheck/v1.0", None)
+
+    print("Status:", status)
+    print("Response:", response)
+
+
+@cli.command()
+def hello(cobalt_api_token):
     """ Print helllo work """
-    click.echo("Hello World")
+    click.echo("Hello World!!!!!")
+    # click.echo(f"--{cobalt_api_token}--")
 
 
-@cli.command()
-@click.option('-n', '--name', type=str, help='Name to greet', default='World')
-def hello(name):
-    click.echo(f'Hello {name}')
+# @cli.command()
+# @click.option('-n', '--name', type=str, help='Name to greet', default='World')
+# def hello(name):
+#     click.echo(f'Hello {name}')
