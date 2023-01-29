ba3f4a716b97294e7832ba0ce151676f33ffef16
Mark Guthrie <m@rkguthrie.com>
2023-01-30 06:38:52 +1100

    starting

--- cobalt_cli.py
+++ cobalt_cli.py
@@ -2,32 +2,45 @@ import click
 
 from api import cobalt_api_call_get
 
-TOKEN = "API_Rm/_XZPkTn2Ez|GYFd)Bj}TPinAhnmXzGwVo"
+# TOKEN=API_Rm/_XZPkTn2Ez|GYFd)Bj}TPinAhnmXzGwVo
 
+
+# @click.group()
+# def cli():
+#     pass
+#
+#
+# @click.command()
+# @click.option('--username', envvar='USERNAME')
+# def greet(username):
+#     click.echo(f"Hello {username}!")
 
 @click.group()
-@click.option('--cobalt-api-token', help='Cobalt API Token, can also be set using an environment variable "COBALT_API_TOKEN"')
-def cli(cobalt_api_token):
+# @click.option('--token', envvar='TOKEN')
+# @click.option('--cobalt-api-token', envvar='COBALT', help='Cobalt API Token, can also be set using an environment variable "COBALT_API_TOKEN"')
+def cli():
     """ Entry point """
     print("Inside cli")
-    print(cobalt_api_token)
+    # print(token)
     print()
     print()
-
-
 
 
 @cli.command()
-def validate_token():
+@click.option('--cobalt-api-token', envvar='COBALT_API_TOKEN')
+def validate_token(cobalt_api_token):
     """ Check that a token is valid """
 
-    status, response = cobalt_api_call_get(TOKEN, "api/cobalt/keycheck/v1.0", None)
+    print("TOKEN:", cobalt_api_token)
+
+    status, response = cobalt_api_call_get(cobalt_api_token, "api/cobalt/keycheck/v1.0", None)
 
     print("Status:", status)
     print("Response:", response)
 
 
 @cli.command()
+@click.option('--cobalt-api-token', envvar='COBALT')
 def hello(cobalt_api_token):
     """ Print helllo work """
     click.echo("Hello World!!!!!")
